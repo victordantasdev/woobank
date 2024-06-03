60cf3bed80366139b5ee3d53ab05880da23bffc1
victordantasdev <victordantasdeveloper@gmail.com>
2024-06-03 00:12:04 -0300

    feat: adds server entrypoint

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,1 +1,18 @@
-console.log("Woobank server...")
+import http from "http"
+import dotenv from "dotenv"
+
+import { app } from "./src/server"
+import { connectToDatabase } from "./src/server/database"
+
+async function main() {
+  dotenv.config({ path: ".env" })
+  const port = process.env.PORT || 4000
+
+  await connectToDatabase()
+  const server = http.createServer(app.callback())
+  server.listen(port, () => {
+    console.log(`Server running on port:${port}`)
+  })
+}
+
+main()
