03724066b5666ce34f305f5e06bae2141a6d57f0
victordantasdev <victordantasdeveloper@gmail.com>
2024-06-03 03:35:08 -0300

    feat: adds login page

---
+++ apps/web/src/pages/LoginPage/validationSchema.ts
@@ -0,0 +1,28 @@
+import z from "zod"
+
+export const LoginSchema = z.object({
+  taxId: z
+    .string({
+      required_error: "CPF/CNPJ é obrigatório.",
+    })
+    .refine((doc) => {
+      const replacedDoc = doc.replace(/\D/g, "")
+      return replacedDoc.length >= 11
+    }, "CPF/CNPJ deve conter no mínimo 11 caracteres.")
+    .refine((doc) => {
+      const replacedDoc = doc.replace(/\D/g, "")
+      return replacedDoc.length <= 14
+    }, "CPF/CNPJ deve conter no máximo 14 caracteres.")
+    .refine((doc) => {
+      const replacedDoc = doc.replace(/\D/g, "")
+      return !!Number(replacedDoc)
+    }, "CPF/CNPJ deve conter apenas números."),
+
+  password: z
+    .string({
+      required_error: "Senha é obrigatória.",
+    })
+    .min(8, "Senha deve conter pelo menos 8 caracteres."),
+})
+
+export type LoginData = z.infer<typeof LoginSchema>
